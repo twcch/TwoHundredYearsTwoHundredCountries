6301d10a071a5bbc2d5245a37b70f283a166cb63
Alan Hsieh <twcch1218@gmail.com>
2025-07-09 11:19:55 +0800

    Add script to generate animated Gapminder visualization using Plotly

---
+++ scripts/plot_with_px.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import plotly.express as px
+from core.sqlite_db import SQLiteDB
+
+
+def main():
+    sqlite_db = SQLiteDB('data/gapminder.db')
+    plotting_df = sqlite_db.query_table_to_dataframe('plotting')
+    sqlite_db.close()
+
+    fig = px.scatter(plotting_df,
+                     x='gdp_per_capita',
+                     y='life_expectancy',
+                     animation_frame='dt_year',
+                     animation_group='country_name',
+                     size='population',
+                     color='continent',
+                     hover_name='country_name',
+                     size_max=100,
+                     range_x=[500, 100000],
+                     range_y=[20, 90],
+                     log_x=True,
+                     title='Gapminder Clone 1800-2023')
+
+    fig.write_html('outputs/gapminder_clone.html', auto_open=True)
+
+
+if __name__ == '__main__':
+    main()
