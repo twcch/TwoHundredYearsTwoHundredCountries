f15e5bc95f20b7ab4de8b50525bc7fe1370d7b04
Alan Hsieh <twcch1218@gmail.com>
2025-07-09 11:19:40 +0800

    Remove unused pandas import from proof_of_concept script

--- scripts/proof_of_concept.py
+++ scripts/proof_of_concept.py
@@ -3,7 +3,6 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from core.sqlite_db import SQLiteDB
